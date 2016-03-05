8065335610ad4487253a8db0d06e8c80fe595d20
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-05 14:45:35 +0300

    Add AJAX response to destroy action

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305070729) do
+ActiveRecord::Schema.define(version: 20160305105541) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "zombie_id"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160305070729) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "message"
+    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
