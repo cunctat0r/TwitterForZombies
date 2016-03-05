d1ff972173e8cd5fdf030d150aaa9eca3dc8c917
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-05 13:53:03 +0300

    Create custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :zombies
+  resources :zombies do
+    resourses :tweets
+    get :decomp, on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
