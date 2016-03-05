b1511eb5a456353531f0c3e570c2982d6ca1b272
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-05 12:43:39 +0300

    Add decomp change notification

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -2,4 +2,12 @@ class Zombie < ActiveRecord::Base
   has_one :brain, dependent: :destroy
   has_many :assignments
   has_many :roles, through: :assignments
+  after_save :decomp_change_notification, if :decomp_changed?
+
+  private
+
+  def decomp_change_notification
+    ZombieMailer.decomp_change(self).deliver
+  end
+
 end
