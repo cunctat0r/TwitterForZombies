82116f3a061347a782f6d849de4b47abb9ad5351
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-05 10:11:42 +0300

    Zombie has_many Roles, Role has_many Zombies

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,4 @@
 class Assignment < ActiveRecord::Base
+  belongs_to :zombie
+  belongs_to :role
 end
--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,3 +1,5 @@
 class Zombie < ActiveRecord::Base
   has_one :brain, dependent: :destroy
+  has_many :assignments
+  has_many :roles, through: :assignments
 end
